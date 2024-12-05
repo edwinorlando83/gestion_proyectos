b0edfea7c312d17367f32e212da3acaf94dde9be
orlando <echolota@sys-nova.com>
2024-12-05 23:06:33 +0000

    cambios

--- gestion_proyectos/hooks.py
+++ gestion_proyectos/hooks.py
@@ -10,6 +10,7 @@ website_context = {
 "favicon": "/assets/gestion_proyectos/favicon.png",
 "splash_image": "/assets/gestion_proyectos/logoSISA.png"
 }
+brand_html = '<div><img src="/assets/gestion_proyectos/sisablanco2.png">  </div>'
 
 permission_query_conditions = {
      
