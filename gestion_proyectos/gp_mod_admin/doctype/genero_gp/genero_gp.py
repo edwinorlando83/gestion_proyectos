b17f62d95bbf5ee882a9810a4c12e72b0e6c7379
orlando <echolota@sys-nova.com>
2025-05-14 07:59:42 -0500

    cambios

---
+++ gestion_proyectos/gp_mod_admin/doctype/genero_gp/genero_gp.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Orlando and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class genero_gp(Document):
+	pass
---
+++ gestion_proyectos/gp_mod_admin/doctype/genero_gp/test_genero_gp.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Orlando and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testgenero_gp(FrappeTestCase):
+	pass
